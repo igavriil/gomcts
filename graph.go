b997f9ad45b16807fb8c6a27d61779b568169c10
Iason Gavriilidis <iasongavriil@gmail.com>
2016-04-17 14:45:24 +0300

    Implements Uninformed (BFS, DFS) graph search algorithms

--- graph.go
+++ graph.go
@@ -16,3 +16,34 @@ func GraphSuccessors(g Graph, s State) []State {
 	}
 	return successors
 }
+
+func UninformedSearch(g Graph, f Frontier) *Node {
+	state := g.InitialState()
+	node := &Node{State: state}
+	if g.GoalTest(state) {
+		return node
+	}
+	f.Push(node)
+	explored := make(map[State]bool)
+	queued := make(map[State]bool)
+
+	for f.Len() > 0 {
+		node = f.Pop().(*Node)
+		state = (*node).State
+		explored[state] = true
+		for _, action := range g.Actions(state) {
+			childNode := node.ChildNode(g, action)
+			state = (*childNode).State
+
+			if e, _ := explored[state]; !e {
+				if q, _ := queued[state]; !q {
+					if g.GoalTest(state) {
+						return childNode
+					}
+					f.Push(childNode)
+					queued[state] = true
+				}
+			}
+		}
+	}
+}
